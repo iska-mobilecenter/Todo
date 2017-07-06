cc40e68413cc5350aaab440111346ae364f185c2
Youp Hulsebos <youp_hulsebos_@hotmail.com>
2017-07-06 13:29:45 +0200

    moved uitest for build

--- Todo.UITest/Features/Toevoegen todo item.feature.cs
+++ Todo.UITest/Features/Toevoegen todo item.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace Todo.Android.UITest.Features
+namespace Todo.UITest.Features
 {
     using TechTalk.SpecFlow;
     
